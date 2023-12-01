38fd58912c8efec7f1bee41a187cc841c35446bd
Ivan-Cosmin <ivancosmin220503@gmail.com>
2023-12-01 11:11:32 +0200

    Make ScreenApp class and initialized the screen.

--- FlyWithPlane/FlyWithPlane.cpp
+++ FlyWithPlane/FlyWithPlane.cpp
@@ -1,20 +1,65 @@
-﻿// FlyWithPlane.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿#include <GL/glew.h>
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <glfw3.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+class ScreenApp {
+public:
+	ScreenApp() {
+		if (!glfwInit()) {
+			throw std::runtime_error("Failed to initialize GLFW");
+		}
+
+		window = glfwCreateWindow(800, 600, "FlyWithPlane", NULL, NULL);
+		if (!window) {
+			glfwTerminate();
+			throw std::runtime_error("Failed to create GLFW window");
+		}
+
+		glfwMakeContextCurrent(window);
+
+		if (glewInit() != GLEW_OK) {
+			throw std::runtime_error("Failed to initialize GLEW");
+		}
+
+		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	}
+
+	void run() {
+		while (!glfwWindowShouldClose(window)) {
+			render();
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+	}
+
+	~ScreenApp() {
+		glfwTerminate();
+	}
+
+private:
+	GLFWwindow* window;
+
+	void render() {
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+};
+
+int main() {
+	try {
+		ScreenApp app;
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+		return -1;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
